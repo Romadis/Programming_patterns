22f3a695c050ec78775a01adc125b6ef2d73406c
Romadis <romadis2024@mail.ru>
2024-09-17 18:36:57 +0300

    Create class Student
    This class contains fields with student's data

---
+++ LR1/Student.kt
@@ -0,0 +1,11 @@
+data class Student(
+    val id: Int,
+    var surname: String,
+    var name: String,
+    var patronymic: String,
+    var phone: String? = null,
+    var telegram: String? = null,
+    var email: String? = null,
+    var git: String? = null
+)
+
