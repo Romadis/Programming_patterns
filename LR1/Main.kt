0aac728b9b82045f5b3bfa048473c38e216f8d24
Romadis <romadis2024@mail.ru>
2024-09-17 23:56:44 +0300

    LR 1.3 - Избавился от повторяющегося кода в конструкторе и аксессорах благодаря преобразованию класса в data class

--- LR1/Main.kt
+++ LR1/Main.kt
@@ -1,12 +1,11 @@
 fun main() {
     val students = mutableListOf<Student>();
     students.add(Student("Романов","Владислав","Витальевич"));
-    students.add(Student("Кривич","Анастасия","Михайловна", studentTelegram = "@mixailovna777"));
-    students.add(Student("Придава","Александр","Александрович", studentGit = "megaladon"));
-    students.add(Student("Полевая","Полина","Андреевна", studentEmail = "polka@gmail.com"));
+    students.add(Student("Кривич","Анастасия","Михайловна", telegram = "@mixailovna777"));
+    students.add(Student("Придава","Александр","Александрович", gitHub = "megaladon"));
+    students.add(Student("Полевая","Полина","Андреевна", email = "polka@gmail.com"));
     students.add(Student("Абрамов","Иван","Дмитриевич"));
     students.add(Student("Гонтарев","Александр","Дмитриевич"));
-    students.add(Student("Нехуженко","Алексей","Денисович"));
     students.add(Student("Попов","Иван","Викторович"));
     students.forEach { it: Student -> println(it) };
 }
