d2a1b24c8c1ce02bd29194d2ad2a105188589511
Romadis <romadis2024@mail.ru>
2024-09-17 23:36:20 +0300

    LR 1.2 - для корректного вывода экземпляра класса добавил метод toString

--- LR1/Main.kt
+++ LR1/Main.kt
@@ -1,6 +1,12 @@
 fun main() {
-    val student = Student("Романов","Владислав","Витальевич");
-    println(student.name);
-    student.name = "Анастасия";
-    println(student.name);
+    val students = mutableListOf<Student>();
+    students.add(Student("Романов","Владислав","Витальевич"));
+    students.add(Student("Кривич","Анастасия","Михайловна", studentTelegram = "@mixailovna777"));
+    students.add(Student("Придава","Александр","Александрович", studentGit = "megaladon"));
+    students.add(Student("Полевая","Полина","Андреевна", studentEmail = "polka@gmail.com"));
+    students.add(Student("Абрамов","Иван","Дмитриевич"));
+    students.add(Student("Гонтарев","Александр","Дмитриевич"));
+    students.add(Student("Нехуженко","Алексей","Денисович"));
+    students.add(Student("Попов","Иван","Викторович"));
+    students.forEach { it: Student -> println(it) };
 }
--- LR1/Student.kt
+++ LR1/Student.kt
@@ -75,13 +75,14 @@ class Student{
         studentSurname:String,
         studentName:String,
         studentPatronymic:String,
+        studentId:Int?= autoGenerateId(),
         studentPhone:String?=null,
         studentTelegram:String?=null,
         studentEmail:String?=null,
         studentGit:String?=null
     )
     {
-        this.id = autoGenerateId();
+        this.id = studentId;
         this.surname = studentSurname;
         this.name = studentName;
         this.patronymic = studentPatronymic;
@@ -90,5 +91,7 @@ class Student{
         this.email = studentEmail;
         this.gitHub = studentGit;
     }
-
+    override fun toString(): String {
+        return "ID:${this.id} Surname:${this.surname} Name:${this.name} Patronymic:${this.patronymic} Phone:${this.phoneNumber} Telegram:${this.telegram} Email:${this.email} Git:${this.gitHub}";
+    }
 }
