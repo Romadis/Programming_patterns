ad1854e3ab9474223381a98b7f887334fb7c8bc2
Romadis <romadis2024@mail.ru>
2024-09-18 00:55:22 +0300

    LR 1.7 - Создал валидацию атрибутов экземпляра класса Student

--- LR1/Main.kt
+++ LR1/Main.kt
@@ -1,12 +1,10 @@
 fun main() {
     val students = mutableListOf<Student>();
     students.add(Student("Романов","Владислав","Витальевич"));
-    students.add(Student("Кривич","Анастасия","Михайловна", telegram = "@mixailovna777"));
-    students.add(Student("Придава","Александр","Александрович", gitHub = "megaladon"));
-    students.add(Student("Полевая","Полина","Андреевна", email = "polka@gmail.com"));
-    students.add(Student("Абрамов","Иван","Дмитриевич"));
-    students.add(Student("Гонтарев","Александр","Дмитриевич"));
-    students.add(Student("Попов","Иван","Викторович"));
-    students.add(Student(hashMapOf(Pair("name", "Владислав"), Pair("surname", "Романов"), Pair("patronymic", "Витальевич"))));
+    students.add(Student("Мамин-Сибиряк","Дмитрий","Наркисович", telegram = "@poet777"));
+    students.add(Student("Махмудов","Арарат-Магомед","Саркисович", gitHub = "abu123"));
+    students.add(Student("Лермонтов","Михаил","Юрьевич", email = "literature@gmail.com"));
+    students.add(Student("Городецкий","Эдуард","Романович", phoneNumber = "+78005553535"));
+    students.add(Student(hashMapOf(Pair("name","Владислав"),Pair("surname","Романов"),Pair("patronymic","Витальевич"))));
     students.forEach { it: Student -> println(it) };
 }
