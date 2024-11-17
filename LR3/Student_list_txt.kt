35e24556690f2f2e58188872386fd23fcbdc9a46
Romadis <romadis2024@mail.ru>
2024-11-17 23:59:31 +0300

    LR 3.4 - Произвел рефакторинг, создав супер класс

--- LR3/Student_list_txt.kt
+++ LR3/Student_list_txt.kt
@@ -1,16 +1,10 @@
 package main.src
-import Data_list
-import main.src.Student.Examination
 import java.io.File
 
-class Students_list_txt(private val filePath: String) {
-    private val students : MutableList<Student> = mutableListOf()
-    init {
-        readFromTxt()
-    }
+class Students_list_txt(filePath: String) : StudentsList(filePath) {
 
     // Чтение файла
-    fun readFromTxt(): List<SuperStudent> {
+    override fun readFromFile() {
         val file = File(filePath)
 
         if (!file.exists() || !file.canRead()) {
@@ -18,18 +12,17 @@ class Students_list_txt(private val filePath: String) {
         }
 
         file.forEachLine { line ->
-            try{
+            try {
                 val student = Student(line)
                 Student.students.add(student)
             } catch (e: IllegalArgumentException) {
                 println("Ошибка: \"$line\"")
             }
         }
-        return Student.students
     }
 
     // Запись в файл
-    fun writeToTxt() {
+    public override fun writeToFile() {
         val file = File("output.txt")
         file.bufferedWriter().use { writer ->
             Student.students.forEach { student ->
@@ -40,54 +33,5 @@ class Students_list_txt(private val filePath: String) {
                 writer.newLine()
             }
         }
-    }
-
-    // Получение объекта класса Student по ID
-    fun getStudentById(id: Int): Student? {
-        return Student.students.find { it.id == id }
-    }
-
-    // Получение списка k по счету n объектов класса Student_short
-    fun get_k_n_student_short_list(n: Int, k: Int): Data_list<Student_short> {
-        val startIndex = (n - 1) * k
-        val endIndex = startIndex + k
-        val studentShortList = Student.students.subList(startIndex.coerceAtLeast(0), endIndex.coerceAtMost(students.size)).map { Student_short(it) }
-        return Data_list(studentShortList)
-    }
-
-    // Сортировка элементов по ФИО
-    fun sortStudents() {
-        Student.students.sortBy { it.getInfo() }
-    }
-
-    // Добавление объектов класса Student в список
-    fun addStudent(student: Student, id: Int = students.maxOf { it.id } + 1) {
-        val newStudent = student
-        newStudent.id = id
-        Student.students.add(newStudent)
-    }
-
-    // Замена элементов списка по ID
-    fun replaceStudentById(id: Int, newStudent: Student) {
-        val newStud = newStudent
-        newStud.id = id
-        val ind = Student.students.indexOf(this.students.find { it.id == id })
-        if (ind != -1) Student.students[ind] = newStud
-        else this.addStudent(newStudent, id)
-    }
-
-    // Удаление элемента списка по ID
-    fun removeStudentById(id: Int) {
-        val index = Student.students.indexOfFirst { it.id == id }
-        if (index != -1) {
-            Student.students.removeAt(index)
-        } else {
-            throw NoSuchElementException("Студент с ID $id не найден.")
-        }
-    }
-
-    // Получение количества элементов
-    fun getStudentShortCount(): Int {
-        return Student.students.size
     }
 }
