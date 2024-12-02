77e946c1338f5f21f8607459f3542c5206e4fe14
Romadis <romadis2024@mail.ru>
2024-12-02 22:33:18 +0300

    LR 4.4 - Заполнил таблицу данными

---
+++ LR4/sql_commands/insert.sql
@@ -0,0 +1,14 @@
+INSERT INTO student (lastname, firstname, middlename, phone, telegram, email, git)
+VALUES ('Романов','Владислав','Витальевич','+79186916943','@romadis','vladcool155@gmail.ru','https://github.com/Romadis');
+
+INSERT INTO student (lastname, firstname, middlename, phone, telegram, email, git)
+VALUES ('Максюта','Станислав','Витальевич','+79184998703','@chokoBoy12345','stanislavsky@gmail.com','https://github.com/Stanislavsky');
+
+INSERT INTO student (lastname, firstname, middlename, telegram, git)
+VALUES ('Харисов','Артур','Марселевич','@artKV','https://github.com/ArtKV1');
+
+INSERT INTO student (lastname, firstname, middlename, phone, telegram, email, git)
+VALUES ('Городецкий','Эдуард','Романович','+78005553535','@edgorod','edgorod777@mail.ru', 'https://github.com/edgorod');
+
+INSERT INTO student (lastname, firstname, middlename, phone, git)
+VALUES ('Кривич','Анастасия','Михайловна','+79002921010','https://github.com/Krivich123');
